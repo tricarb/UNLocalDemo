ce15299476c62b15160c1a0cfc77c6ae8ab22332
Al Malin <amalin@mac.com>
2020-06-17 10:28:38 -0400

    Initial Commit

---
+++ UNLocalDemo/ViewController.swift
@@ -0,0 +1,20 @@
+////
+//  ViewController.swift
+//  UNLocalDemo
+//
+//  Created by Al Malin (tricarb) on 6/17/20.
+//  Copyright © 2020 Albert A. Malin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
